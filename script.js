6af9f177ea64674a6e3bbec58a72cf95e8b6ef94
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-10 20:42:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 
 let start =0;
 const increment = function () {
-	
+	alert(start);
+	start++;
+	document.getElementById('counter').innerText=start;	
 }
 
 document.querySelector('button').addEventListener('click',()=>{
